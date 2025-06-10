80037a92e1edfcb257ff4c80dc2e2245d6809fd4
Davy Dantas <davy.dantas@escolar.ifrn.edu.br>
2025-06-10 12:21:57 -0300

    :spakles:feat: projects pages

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -2,12 +2,12 @@ import { Flex, Tabs } from "@chakra-ui/react"
 import { ColorModeButton } from "../ui/color-mode"
 export default function Nav() {
     return (
-        <Flex as="nav" padding={4} width={"100%"} justifyContent={"center"}>
+        <Flex as="nav" padding={4} marginBottom={100} width={"100%"} justifyContent={"center"}>
             <Tabs.Root width={"100%"} lazyMount unmountOnExit defaultValue="tab-1">
                 <Tabs.List alignContent={"center"} gap={8} justifyContent={"center"} borderBottomWidth={0}>
-                    <Tabs.Trigger value="tab-1">Tab 1</Tabs.Trigger>
-                    <Tabs.Trigger value="tab-2">Tab 2</Tabs.Trigger>
-                    <Tabs.Trigger value="tab-3">Tab 3</Tabs.Trigger>
+                    <Tabs.Trigger value="tab-1">About Me</Tabs.Trigger>
+                    <Tabs.Trigger value="tab-2">Projects</Tabs.Trigger>
+                    <Tabs.Trigger value="tab-3">Contacts</Tabs.Trigger>
                 </Tabs.List>
             </Tabs.Root>
             <ColorModeButton/>
